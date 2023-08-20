f7bcb4be184606abeb3eafc9f4dc42dac8433019
Miftah Salam <salam.miftah@gmail.com>
2023-08-20 07:11:32 +0700

    add json property name

--- src/main/java/com/example/fooddeliverysystem/model/dto/AddMealDTO.java
+++ src/main/java/com/example/fooddeliverysystem/model/dto/AddMealDTO.java
@@ -2,15 +2,17 @@ package com.example.fooddeliverysystem.model.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record AddMealDTO(
-        @NotBlank String name,
-        String description,
-        @NotNull boolean earlyOrder,
-        @NotNull boolean isReguler,
-        @NotNull() @NotEmpty() List<TypeDTO> typeEntities) {
+                @NotBlank String name,
+                String description,
+                @NotNull boolean earlyOrder,
+                @NotNull boolean isReguler,
+                @JsonProperty("types") @NotNull() @NotEmpty() List<TypeDTO> typeEntities) {
 
 }
